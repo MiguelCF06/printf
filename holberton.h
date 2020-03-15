b87bfa454d7b8823612f67cf5cd0d0086610ba3d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-15 22:41:00 +0000

    Added prototypes

--- holberton.h
+++ holberton.h
@@ -1,8 +1,26 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
+/**
+ * struct formatos - Struct of the different formats of printf
+ *
+ * @ptr: The operator
+ * @f: The function associated
+ */
+typedef struct formats
+{
+	char *ptr;
+	int (*f)(va_list);
+
+}fmt;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
-
-
+int print_char(va_list c);
+int print_str(va_list s);
 
 #endif
