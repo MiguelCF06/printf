f225be87ceedbe242070e649447973b0700b3307
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-15 22:40:30 +0000

    Added functions to print chars and strings

---
+++ print_functions.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_char - Print characters
+ * @c: type data
+ * Return always 1
+ */
+int print_char(va_list c)
+{
+	char caracter = (char)va_arg(c, int);
+
+	_putchar(caracter);
+	return (1);
+}
+/**
+ * print_str - prints a string
+ * @s: the string to print
+ * Return: number of characters printed
+ */
+int print_str(va_list s)
+{
+	unsigned int x;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(Null)";
+
+	for (x = 0; str[x]; x++)
+	{
+		_putchar(str[x]);
+	}
+	return (x);
+}
